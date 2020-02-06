0fd17cc216ed53d2d3ec1485f8cb4e1ecf6a6901
Mjuksel <mportma@gmail.com>
2020-02-06 16:55:46 +0100

    Update visitor.js

--- js/visitor.js
+++ js/visitor.js
@@ -3,9 +3,9 @@ $(function() {
   const testChildren = cont.children().length;
   const txt = marked(cont.text());
   /*
-   * check if content has children.
-   * should (?) only have plain text in Markdown formatting
-   * finally render content + hide source area.
+   * Check if content has children.
+   * Should (?) only have plain text in Markdown formatting
+   * Finally render content + hide source area.
    */
   if (testChildren === 0) {
     cont.before(`<div class="markdown-body">${txt}</div>`).hide();
